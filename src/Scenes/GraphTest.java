ea2a604044ab34b6084d469633eefdb3ddca6878
= <trailcapital@gmail.com>
2016-12-11 14:18:42 -0500

    added comments to graphTest to explain changes

--- src/Scenes/GraphTest.java
+++ src/Scenes/GraphTest.java
@@ -28,18 +28,27 @@ public class GraphTest extends Application{
     	
         stage.setTitle("Comp3203");
         
+        //BEGINNING OF CHANGES MADE BY MATTHEW
+        
+        //NOTE: Must include Split Algorithm in final version
+        
+        //The following variables are the input needed by AlgorithmController to generate the hash map
         int runTimes = 200;
         int numSensors = 7;
         double radius = 0.1;
         
+        //Must create an AlgorithmController for each algorithm type
         AlgorithmController rigid = new AlgorithmController("RIGID_COVERAGE");
         AlgorithmController simple = new AlgorithmController("SIMPLE_COVERAGE");
         AlgorithmController overlap = new AlgorithmController("OVERLAP_COVERAGE");
         
+        //Call buildHashMap on each AlgorithmController with respective parameters to get respective hash map. 
         GraphScene graph = new GraphScene();
         graph.getGraph().createSeries("RigidAlgorithm",rigid.buildHashMap(runTimes, numSensors, radius));
         graph.getGraph().createSeries("SimpleAlgorithm",simple.buildHashMap(runTimes, numSensors, radius));
         graph.getGraph().createSeries("OverlapAlgorithm",overlap.buildHashMap(runTimes, numSensors, radius));
+        
+       //END OF CHANGES MADE BY MATTHEW
         
         graph.getGraph().setNumSensor(10);
         stage.setScene(graph.getGraphScene());
